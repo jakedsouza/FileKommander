5bd9bca8565ee80c685b68a1f7fe0d5f5dc05b64
Jake Dsouza <jakedsouza88@gmail.com>
2012-11-13 08:18:36 -0500

    added dirty code will change later . run to see our annotations :)

--- src/com/poly/nlp/filekommander/FileKommander.java
+++ src/com/poly/nlp/filekommander/FileKommander.java
@@ -1,10 +1,16 @@
 package com.poly.nlp.filekommander;
 
+import gate.Corpus;
+import gate.Document;
+import gate.Factory;
+import gate.creole.SerialAnalyserController;
 import gate.util.GateException;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.Properties;
@@ -30,20 +36,75 @@ public class FileKommander {
 	
 	
 	
-	public static void main(String[] args) throws MalformedURLException, GateException, ConfigurationException {
+	public static void main(String[] args) throws GateException, ConfigurationException, IOException {
 	
 		// Load properties file 
-//		log.info("Reading Properties File");		
-//		Configuration config = null ;
-//		try {
-//			config = new PropertiesConfiguration("FileKommander.properties");
-//		} catch (ConfigurationException e) {
-//			log.error("Error reading properties file", e);
-//		}
-//		
-//		String [] pr_list = config.getStringArray("PR_LIST");
-//		System.out.println();
+		log.info("Reading Properties File");		
+		Configuration config = null ;
+		try {
+			config = new PropertiesConfiguration("FileKommander.properties");
+		} catch (ConfigurationException e) {
+			log.error("Error reading properties file", e);
+		}
+		
+		String gateHome = config.getString("gateHome");
+		String gazetteerFilePath = config.getString("gazetteerFilePath");
+		String japeFilePath = config.getString("japeFilePath");
+		
+		
+		GateBuilder gateBuilder = new GateBuilder(gateHome,gazetteerFilePath,japeFilePath);
+		gateBuilder.setup();
+		gateBuilder.loadANNIE();
+		gateBuilder.loadAllProcessingResources();
+		SerialAnalyserController a = gateBuilder.getAnnieController();
 
+		File file = new File("language resources/corpus/GoodCorp.txt") ;
+		Document doc = Factory.newDocument(getContents(file));
+		Corpus corpus =Factory.newCorpus("BatchProcessApp Corpus");;
+		corpus.add(doc);
+		a.setCorpus(corpus);
+		a.execute();
+		corpus.clear();  
+	
+	}
+	
+	
+	public static String getContents(File aFile) {
+		// ...checks on aFile are elided
+		StringBuffer contents = new StringBuffer();
+
+		// declared here only to make visible to finally clause
+		BufferedReader input = null;
+		try {
+			// use buffering, reading one line at a time
+			// FileReader always assumes default encoding is OK!
+			input = new BufferedReader(new FileReader(aFile));
+			String line = null; // not declared within while loop
+			/*
+			 * readLine is a bit quirky : it returns the content of a line MINUS
+			 * the newline. it returns null only for the END of the stream. it
+			 * returns an empty String if two newlines appear in a row.
+			 */
+			while ((line = input.readLine()) != null) {
+				contents.append(line);
+				contents.append(System.getProperty("line.separator"));
+			}
+		} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			try {
+				if (input != null) {
+					// flush and close both "input" and its underlying
+					// FileReader
+					input.close();
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return contents.toString();
 	}
 
 }
--- src/com/poly/nlp/filekommander/gate/GateBuilder.java
+++ src/com/poly/nlp/filekommander/gate/GateBuilder.java
@@ -4,14 +4,22 @@
 package com.poly.nlp.filekommander.gate;
 
 import gate.Factory;
+import gate.FeatureMap;
 import gate.Gate;
+import gate.ProcessingResource;
+import gate.creole.ResourceInstantiationException;
 import gate.creole.SerialAnalyserController;
+import gate.creole.Transducer;
+import gate.creole.annotdelete.AnnotationDeletePR;
+import gate.creole.gazetteer.DefaultGazetteer;
 import gate.util.GateException;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.apache.log4j.Logger;
 
@@ -27,59 +35,176 @@ public class GateBuilder {
 	public static String newline = ("\n");
 	private static final Logger log = Logger.getLogger(GateBuilder.class);
 
-	private String GATE_HOME;
-	private URI ANNIE_HOME ;
+	private String gateHome;
+	private URI annieHome;
+	private String japeFilePath;
+	private String gazetteerFilePath;
+
 	private String ANNIE_CLASS = "gate.creole.SerialAnalyserController";
-	
-	private SerialAnalyserController annieController;
+
 	private ArrayList<ProcessingResource> prList;
-	private ArrayList<String> prListString;
-	public GateBuilder() throws GateException, MalformedURLException {
+	private SerialAnalyserController annieController;
 
-		setup();
+	public GateBuilder() {
+		
 	}
-	
 
-	public void setup() throws GateException, MalformedURLException {
-		if (this.GATE_HOME == null) {
+	public GateBuilder(String gateHome, String gazetteerFilePath,
+			String japeFilePath) {
+		this.prList = new ArrayList<>();
+		this.gateHome = gateHome;
+		this.gazetteerFilePath = gazetteerFilePath;
+		this.japeFilePath = japeFilePath;
+
+		if (this.gateHome == null) {
 			log.warn("GATE_HOME is not set trying to find GATE_HOME in sysenv");
-			this.GATE_HOME = System.getenv("GATE_HOME");
-			if (this.GATE_HOME == null) {
+			this.gateHome = System.getenv("GATE_HOME");
+			if (this.gateHome == null) {
 				log.error("GATE_HOME can not be found will exit ");
 				throw new RuntimeException("GATE_HOME not set");
 			}
 
 		}
-		
-		this.ANNIE_HOME = new File(GATE_HOME+ fileSeparator
-				+ "plugins" + fileSeparator + "ANNIE").toURI();
 
-		System.setProperty("gate.home", GATE_HOME);
+		this.annieHome = new File(this.gateHome + fileSeparator + "plugins"
+				+ fileSeparator + "ANNIE").toURI();
+		System.setProperty("gate.home", this.gateHome);
 		System.setProperty("gate.site.config", System.getProperty("gate.home")
 				+ fileSeparator + "gate.xml");
 		System.setProperty("gate.user.config",
 				System.getProperty("gate.site.config"));
+	}
 
-		
-		
+	public void setup() throws GateException, MalformedURLException {
 		log.info("Initialising GATE");
 		Gate.init();
 		log.info("Done initialising GATE");
-		
+
 		// register plugin directories
-		Gate.getCreoleRegister().registerDirectories(ANNIE_HOME.toURL());
-		
-		// start annie
-		
-		annieController = (SerialAnalyserController) Factory
-				.createResource(ANNIE_CLASS, Factory
-						.newFeatureMap(), Factory.newFeatureMap(), "ANNIE_"
-						+ Gate.genSym());	
-		
-		for (String pr : prListString) {
-			
+		Gate.getCreoleRegister().registerDirectories(annieHome.toURL());
+
+	}
+
+	public void loadANNIE() throws ResourceInstantiationException {
+		annieController = (SerialAnalyserController) Factory.createResource(
+				ANNIE_CLASS, Factory.newFeatureMap(), Factory.newFeatureMap(),
+				"ANNIE_" + Gate.genSym());
+
+		return;
+	}
+
+	public void loadAllProcessingResources()
+			throws ResourceInstantiationException, MalformedURLException {
+
+		FeatureMap params = Factory.newFeatureMap();
+		params.put("keepOriginalMarkupsAS", true);
+		ProcessingResource documentReserPR = (ProcessingResource) Factory
+				.createResource("gate.creole.annotdelete.AnnotationDeletePR",
+						params);
+		prList.add(documentReserPR);
+
+		params = Factory.newFeatureMap();
+		ProcessingResource simpleTokeniser = (ProcessingResource) Factory
+				.createResource("gate.creole.tokeniser.SimpleTokeniser", params);
+		prList.add(simpleTokeniser);
+
+		params = Factory.newFeatureMap();
+		ProcessingResource sentenceSplitter = (ProcessingResource) Factory
+				.createResource("gate.creole.splitter.SentenceSplitter", params);
+		prList.add(sentenceSplitter);
+
+		params = Factory.newFeatureMap();
+		params.put("baseSentenceAnnotationType", "Sentence");
+		params.put("baseTokenAnnotationType", "Token");
+		params.put("outputAnnotationType", "Token");
+		ProcessingResource pos = (ProcessingResource) Factory.createResource(
+				"gate.creole.POSTagger", params);
+		prList.add(pos);
+
+		params = Factory.newFeatureMap();
+		// URL u=new
+		// File("ie/deri/nlp/LanguageResources/gazetteers/mylists.def").toURL();
+		params.put("listsURL", new File(gazetteerFilePath).toURI().toURL());
+		DefaultGazetteer gazetteer = (DefaultGazetteer) Factory.createResource(
+				"gate.creole.gazetteer.DefaultGazetteer", params);
+		prList.add(gazetteer);
+
+		params = Factory.newFeatureMap();
+		params.put("grammarURL", new File(japeFilePath).toURI().toURL());
+		Transducer relationExtractionTransducer = (Transducer) Factory
+				.createResource("gate.creole.Transducer", params);
+		prList.add(relationExtractionTransducer);
+		if (annieController == null) {
+			log.warn("Annie is not loaded");
+		} else {
+			for (Iterator<ProcessingResource> iterator = prList.iterator(); iterator
+					.hasNext();) {
+
+				annieController.add(iterator.next());
+
+			}
 		}
-		
+
+	}
+
+	/**
+	 * @return the gateHome
+	 */
+	public String getGateHome() {
+		return gateHome;
+	}
+
+	/**
+	 * @param gateHome
+	 *            the gateHome to set
+	 */
+	public void setGateHome(String gateHome) {
+		this.gateHome = gateHome;
+	}
+
+	/**
+	 * @return the japeFilePath
+	 */
+	public String getJapeFilePath() {
+		return japeFilePath;
+	}
+
+	/**
+	 * @param japeFilePath
+	 *            the japeFilePath to set
+	 */
+	public void setJapeFilePath(String japeFilePath) {
+		this.japeFilePath = japeFilePath;
+	}
+
+	/**
+	 * @return the gazetteerFilePath
+	 */
+	public String getGazetteerFilePath() {
+		return gazetteerFilePath;
+	}
+
+	/**
+	 * @param gazetteerFilePath
+	 *            the gazetteerFilePath to set
+	 */
+	public void setGazetteerFilePath(String gazetteerFilePath) {
+		this.gazetteerFilePath = gazetteerFilePath;
+	}
+
+	/**
+	 * @return the annieController
+	 */
+	public SerialAnalyserController getAnnieController() {
+		return annieController;
+	}
+
+	/**
+	 * @param annieController
+	 *            the annieController to set
+	 */
+	public void setAnnieController(SerialAnalyserController annieController) {
+		this.annieController = annieController;
 	}
 
 }
